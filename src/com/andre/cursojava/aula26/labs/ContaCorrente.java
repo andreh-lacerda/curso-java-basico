559420800e677aa532cc94e3e4a8a05dad2947bb
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-05-08 10:20:51 -0300

    feat: Exer 02

---
+++ src/com/andre/cursojava/aula26/labs/ContaCorrente.java
@@ -0,0 +1,43 @@
+package com.andre.cursojava.aula26.labs;
+
+public class ContaCorrente {
+	
+	String numero;
+	String agencia;
+	double saldo;
+	boolean especial;
+	double limiteEspecial;
+	double valorEspecialUsado;
+	
+	boolean realizarSaque(double quantiaASacar) {
+		
+		if ( saldo >= quantiaASacar) {
+			saldo -= quantiaASacar;
+			return true;
+		} else if (especial) {
+			double limite = limiteEspecial + saldo;
+			if ( limite >= quantiaASacar) {
+				saldo -= quantiaASacar;
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	void depositar(double valorDepositado) {
+		
+		saldo += valorDepositado;
+	}
+	
+	void consultarSaldo() {
+		System.out.println("Saldo atual da conta = " + saldo);
+	}
+	
+	boolean verificarUsoChequeEspecial() {
+		return saldo < 0;
+	}
+
+}
---
+++ src/com/andre/cursojava/aula26/labs/Exer02.java
@@ -0,0 +1,54 @@
+package com.andre.cursojava.aula26.labs;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente contaCorrente = new ContaCorrente();
+		
+		contaCorrente.agencia = "1234";
+		contaCorrente.numero = "123456";
+		contaCorrente.especial = true;
+		contaCorrente.limiteEspecial = 500;
+		contaCorrente.valorEspecialUsado = 0;
+		contaCorrente.saldo = -10;
+		
+		boolean saqueEfetuado = contaCorrente.realizarSaque(10);
+		
+		if (saqueEfetuado) {
+			System.out.println("Saque efetuado com sucesso");
+			contaCorrente.consultarSaldo();
+		} else {
+			System.out.println("Não foi possível realizar saque. Saldo insuficiente");
+		}
+		
+		saqueEfetuado = contaCorrente.realizarSaque(500);
+		System.out.println("Tentativa de saque de 500 reais");
+		if (saqueEfetuado) {
+			System.out.println("Saque efetuado com sucesso");
+			contaCorrente.consultarSaldo();
+		} else {
+			System.out.println("Não foi possível realizar saque. Saldo insuficiente");
+		}
+		
+		System.out.println("Depósito de 500 reais");
+		contaCorrente.depositar(500);
+		contaCorrente.consultarSaldo();
+		
+		if (contaCorrente.verificarUsoChequeEspecial()) {
+			System.out.println("Está utilizando cheque especial");
+		} else {
+			System.out.println("Não está utilizando cheque especial");
+		}
+		
+		contaCorrente.realizarSaque(600);
+		contaCorrente.consultarSaldo();
+		if (contaCorrente.verificarUsoChequeEspecial()) {
+			System.out.println("Está utilizando cheque especial");
+		} else {
+			System.out.println("Não está utilizando cheque especial");
+		}
+
+	}
+
+}
