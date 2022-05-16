f5d53945decf7204f10a0303032e313be8abed18
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-05-16 09:26:29 -0300

    feat: Aulas 37-43 e Exer02

---
+++ src/com/andre/cursojava/aula37ate43/labs/exer02/PessoaJuridica.java
@@ -0,0 +1,32 @@
+package com.andre.cursojava.aula37ate43.labs.exer02;
+
+public class PessoaJuridica extends Contribuinte {
+	
+	private String cnpj;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public double calcularImposto() {	
+		return this.getRendaBruta() * 0.1;
+	}
+
+	@Override
+	public String toString() {
+		String s = "Pessoa Jurídica[";
+		s += super.toString();
+		s += " ;cnpj: " + cnpj;
+		s += " ;imposto a ser pago: " + calcularImposto();
+		s += "]";
+		return s;
+	}
+	
+	
+
+}
