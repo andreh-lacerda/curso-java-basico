588034557db442d9619429f87ba9b8b82d9bd4e9
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-05-17 18:38:34 -0300

    feat: Exer sobre aulas 44-46

---
+++ src/com/andre/cursojava/aula46/labs/DimensaoSuperficial.java
@@ -0,0 +1,7 @@
+package com.andre.cursojava.aula46.labs;
+
+public interface DimensaoSuperficial {
+	
+	double calcularArea();
+
+}
---
+++ src/com/andre/cursojava/aula46/labs/DimensaoVolumetrica.java
@@ -0,0 +1,7 @@
+package com.andre.cursojava.aula46.labs;
+
+public interface DimensaoVolumetrica {
+	
+	double calcularVolume();
+
+}
