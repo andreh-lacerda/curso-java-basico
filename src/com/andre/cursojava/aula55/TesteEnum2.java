6793c61e011ca7aa8d2348ed792c49feb831c91c
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-05-23 08:59:08 -0300

    feat: aula 55 Enum

---
+++ src/com/andre/cursojava/aula55/TesteEnum2.java
@@ -0,0 +1,18 @@
+package com.andre.cursojava.aula55;
+
+import com.andre.cursojava.aula53.DiaSemana;
+
+public class TesteEnum2 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Enum.valueOf(DiaSemana.class, "DOMINGO"));
+		
+		DiaSemana dia = Enum.valueOf(DiaSemana.class, "DOMINGO");
+		
+		System.out.println(dia);
+
+	}
+
+}
