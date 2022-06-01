a4bc10114b641134ab9c475abeca53f4c8ae9a67
André Lacerda <66031421+andreh-lacerda@users.noreply.github.com>
2022-06-01 17:50:35 -0300

    feat: Aulas 80 a 84

---
+++ src/com/andre/cursojava/aula75_84/Aula81.java
@@ -0,0 +1,17 @@
+package com.andre.cursojava.aula75_84;
+
+public class Aula81 {
+
+	public static void main(String[] args) {
+		
+		String teste = "Teste";
+		
+		String testeMinusc = teste.toLowerCase();
+		String testeMaiusc = teste.toUpperCase();
+		
+		System.out.println(testeMinusc);
+		System.out.println(testeMaiusc);
+
+	}
+
+}
